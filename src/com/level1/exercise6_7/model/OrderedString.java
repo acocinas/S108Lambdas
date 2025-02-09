8a60536a95c8881ab205bd1b656637f032821af4
acocinas <alcogar79@gmail.com>
2025-02-09 18:52:45 +0100

    adding exercise7, from highest to lowest

--- src/com/level1/exercise6_7/model/OrderedString.java
+++ src/com/level1/exercise6_7/model/OrderedString.java
@@ -17,7 +17,7 @@ public class OrderedString {
         list.add("y punto");
         list.add(1986);
 
-        System.out.println("Imprimimos la lista entera antes de filtrar: " + list);
+        System.out.println("\nImprimimos la lista entera antes de filtrar:\n" + list);
 
         //Exercise6
         List<String> orderedString = list.stream()
@@ -26,9 +26,18 @@ public class OrderedString {
                 .sorted((s1 , s2) -> Integer.compare(s1.length(), s2.length()))
                 .collect(Collectors.toList());
 
-        System.out.println("Ahora imprimimos las cadenas ordenadas de mayor a menor");
+        System.out.println("\nAhora imprimimos las cadenas ordenadas de mayor a menor, ejercicio 6");
         orderedString.forEach(System.out::println);
 
+        //Exercise7
+        List<String> orderedString2 = list.stream()
+                .filter(obj -> obj instanceof String)
+                .map(obj -> (String) obj)
+                .sorted((s1 , s2) -> Integer.compare(s2.length(), s1.length()))
+                .collect(Collectors.toList());
+
+        System.out.println("\nAhora imprimimos las cadenas ordenadas de menor a mayor, ejercicio 7");
+        orderedString2.forEach(System.out::println);
 
     }
 }
